cfa360d22e698313a7b81cc1bcb04cb94579e076
Michael Putnam <mikeputnam713@gmail.com>
2022-03-22 21:23:00 -0700

    Add game class functionality

--- src/Game.js
+++ src/Game.js
@@ -1,9 +1,16 @@
+const Card = require('./Card');
 const data = require('./data');
+const Deck = require('./Deck');
+const Round = require('./Round');
 const prototypeQuestions = data.prototypeData;
 const util = require('./util');
 
 class Game {
-  constructor() {}
+  constructor(round) {
+    this.currentRound = round;
+    this.cards = []
+    this.deck = {}
+  }
 
   printMessage(deck, round) {
     console.log(`Welcome to FlashCards! You are playing with ${deck.countCards()} cards.
@@ -12,6 +19,16 @@ class Game {
 
   printQuestion(round) {
       util.main(round);
+  }
+
+  start() {
+   this.cards = prototypeQuestions.map((card) => 
+      new Card(card.id, card.question, card.answers, card.correctAnswer)
+    )
+    this.deck = new Deck(this.cards)
+    this.currentRound = new Round(this.deck)
+    this.printMessage(this.deck, this.currentRound)
+    this.printQuestion(this.currentRound)
   }
 }
 
